77a2a9961e3bce0e1f656a81b456ecbe7fa03e16
syslink <oursyslink@163.com>
2020-02-29 00:46:50 +0800

    init

---
+++ README.md
@@ -0,0 +1,68 @@
+## 使用方法
+
+##### 1，使用go命令下载unichain代码，同时下载本代码
+
+命令  
+
+```
+go get github.com/unichainplatform/unichain
+go get github.com/unichainplatform/faucet4testnet
+```
+
+
+##### 2，切换到当前目录，编译代码
+
+```
+cd path/to/github.com/unichainplatform/faucet4testnet
+go build faucet4testnet.go
+```
+
+##### 3，创建程序使用的多个子账户
+
+程序使用多个自账户轮流发送交易。所以在启动这个程序前需要先创建好账户。  
+默认使用账户名 walletservice.u1~9 九个账户轮流发送交易  
+这个账户名可在启动程序时，通过参数 -pn进行设置  
+
+##### 4，启动程序提供http接口服务
+
+启动程序并传入相应的参数  
+
+```
+./faucet4testnet -pn walletservice.u -pk xxxxxxxx -l 5 -p 33000
+```
+
+参数解释：  
+-pn 用于创建账户的账户名前缀，实际发送交易时会在最后加上一个1～9的数字  
+-pk 发送交易的账户私钥，这里认为pn传入的账户使用同一私钥。  
+-l 每个ip创建账户的数量限制。  
+-p 指定的rpc节点端口，此处使用端口区分，主网与测试网。使用的是一个同时提供主网和测试网的rpc节点
+
+调试完毕可以做为后台服务启动  
+
+```
+nohup ./faucet4testnet -pn walletservice.u -pk xxxxxxxx -l 5 -p 33000 &
+```
+
+##### 5，调用服务的http接口
+
+默认只能通过本地localhost访问
+
+```
+http://localhost:12345/wallet_account_creation?accname=arg1&pubkey=0xXXXXXXXXX&deviceid=deviceid
+```
+
+参数：  
+accname 新建账户名称  
+pubkey 新建账户公钥  
+deviceid 设备id字串  
+
+成功则返回交易hash  
+```
+{"code":200,"msg":"0xXXXXXXXXXXXXXX"}
+```
+
+##### 6，代码修改说明  
+
+- 文件 138 ～ 145行，设置rpc节点ip与chainID。 chainID 1为主网，100为测试网。
+- 文件 254 行，调整轮流发送交易账户个数
+- 文件 306 行，调整本地服务监听端口
---
+++ faucetServer.go
@@ -0,0 +1,291 @@
+package faucet4testnet
+
+import (
+"crypto/ecdsa"
+"crypto/sha256"
+"encoding/json"
+"flag"
+"fmt"
+"math/big"
+"strconv"
+"strings"
+
+"github.com/unichainplatform/unichain/accountmanager"
+"github.com/unichainplatform/unichain/common"
+"github.com/unichainplatform/unichain/crypto"
+"github.com/unichainplatform/unichain/types"
+"github.com/unichainplatform/unichain/utils/rlp"
+
+"github.com/syndtr/goleveldb/leveldb"
+"github.com/syndtr/goleveldb/leveldb/errors"
+"github.com/syndtr/goleveldb/leveldb/filter"
+"github.com/syndtr/goleveldb/leveldb/opt"
+
+tc "github.com/unichainplatform/unichain/test/common"
+"net/http"
+)
+
+var (
+	gaslimit = uint64(20000000)
+)
+
+type GenAction struct {
+	*types.Action
+	PrivateKey *ecdsa.PrivateKey
+}
+
+func createAccount(accountName, from common.Name, nonce uint64, publickey common.PubKey, prikey *ecdsa.PrivateKey, chain_id int) (error, common.Hash) {
+	account := &accountmanager.CreateAccountAction{
+		AccountName: accountName,
+		Founder:     common.Name(""),
+		PublicKey:   publickey,
+		Description: "create by unichain wallet",
+	}
+	payload, err := rlp.EncodeToBytes(account)
+	if err != nil {
+		return fmt.Errorf("rlp payload err %v", err), common.Hash{}
+	}
+	gc := newGeAction(types.CreateAccount, from, "unichain.account", nonce, 0, gaslimit, nil, payload, prikey)
+	var gcs []*GenAction
+	gcs = append(gcs, gc)
+	return sendTxTest(gcs, chain_id)
+}
+
+func GeneragePubKey() (common.PubKey, *ecdsa.PrivateKey) {
+	prikey, _ := crypto.GenerateKey()
+	return common.BytesToPubKey(crypto.FromECDSAPub(&prikey.PublicKey)), prikey
+}
+
+func newGeAction(at types.ActionType, from, to common.Name, nonce uint64, assetid uint64, gaslimit uint64, amount *big.Int, payload []byte, prikey *ecdsa.PrivateKey) *GenAction {
+	action := types.NewAction(at, from, to, nonce, assetid, gaslimit, amount, payload, nil)
+
+	return &GenAction{
+		Action:     action,
+		PrivateKey: prikey,
+	}
+}
+
+func sendTxTest(gcs []*GenAction, chain_id int) (error, common.Hash) {
+	signer := types.NewSigner(big.NewInt(int64(chain_id)))
+	var actions []*types.Action
+	for _, v := range gcs {
+		actions = append(actions, v.Action)
+	}
+	tx := types.NewTransaction(uint64(0), big.NewInt(1000000000), actions...)
+	for _, v := range gcs {
+		keypair := types.MakeKeyPair(v.PrivateKey, []uint64{0})
+		err := types.SignActionWithMultiKey(v.Action, tx, signer, 0, []*types.KeyPair{keypair})
+		if err != nil {
+			return fmt.Errorf("SignAction err %v", err), common.Hash{}
+		}
+
+	}
+	rawtx, err := rlp.EncodeToBytes(tx)
+	if err != nil {
+		return err, common.Hash{}
+	}
+	hash, err := tc.SendRawTx(rawtx)
+	if err != nil {
+		return err, common.Hash{}
+	}
+	fmt.Printf("hash: %x", hash)
+	return nil, hash
+}
+
+type RespForm struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg,omitempty"`
+}
+
+type DbRecord struct {
+	Count uint `json:"count"`
+}
+
+type DbStatus struct {
+	Pos int `json:"pos"`
+}
+
+var db_status_key = []byte("db_status_key")
+var pn = flag.String("pn", "walletservice.u", "user name")
+var pk = flag.String("pk", "", "priv key")
+var climit = flag.String("l", "5", "create limit per user")
+var port = flag.String("p", "33100", "port")
+
+func main() {
+	flag.Parse()
+
+	na := *pn
+	pri := *pk
+	prikey, _ := crypto.HexToECDSA(pri)
+
+	cl, _ := strconv.Atoi(*climit)
+	url := "http://120.92.115.77:" + *port
+
+	var chain_id int
+	if "33100" == *port {
+		chain_id = 1
+	} else if "33000" == *port {
+		chain_id = 100
+	}
+
+	fmt.Printf("user_name:%v priv_key:%v climit:%v \n", na, pri, cl)
+	fmt.Printf("url:%v chain_id:%v \n", url, chain_id)
+
+	// level db
+	db_path := "./ldb/"
+
+	//os.RemoveAll(db_path)
+	db, err := leveldb.OpenFile(db_path, &opt.Options{
+		OpenFilesCacheCapacity: 16,
+		BlockCacheCapacity:     8 * opt.MiB,
+		WriteBuffer:            4 * opt.MiB,
+		Filter:                 filter.NewBloomFilter(100),
+	})
+	if _, corrupted := err.(*errors.ErrCorrupted); corrupted {
+		db, err = leveldb.RecoverFile(db_path, nil)
+	}
+	defer db.Close()
+
+	http.HandleFunc("/wallet_account_creation", func(w http.ResponseWriter, r *http.Request) {
+		q := r.URL.Query()
+		resform := &RespForm{
+			Code: 200,
+		}
+
+		// this is a for-break imp
+		for {
+			var accname string
+			var pubkey string
+			var deviceid string
+
+			if val, ok := q["accname"]; ok {
+				accname = val[0]
+			} else {
+				resform.Code = 400
+				resform.Msg = "accname miss!"
+				break
+			}
+
+			if val, ok := q["pubkey"]; ok {
+				pubkey = val[0]
+			} else {
+				resform.Code = 400
+				resform.Msg = "pubkey miss!"
+				break
+			}
+
+			if val, ok := q["deviceid"]; ok {
+				deviceid = val[0]
+			} else {
+				resform.Code = 400
+				resform.Msg = "deviceid miss!"
+				break
+			}
+
+			// X-Real-IP from header
+			var remote_addr string
+			var ip_str string
+			if x_real_ip, ok := r.Header["X-Forwarded-For"]; ok {
+				//do something here
+				ip_str= x_real_ip[0]
+			}else{
+				remote_addr = r.RemoteAddr
+				idx := strings.Index(remote_addr,":")
+				ip_str=remote_addr[:idx]
+			}
+			//fmt.Printf("%v\n",r.Header)
+
+			// ip limit check
+			db_key := sha256.Sum256([]byte(ip_str))
+
+			db_record := DbRecord{}
+			if db_value, err := db.Get(db_key[:], nil); err != nil {
+				if err != errors.ErrNotFound {
+					resform.Code = 500
+					resform.Msg = err.Error()
+					break
+				}
+			} else {
+				json.Unmarshal(db_value, &db_record)
+			}
+			fmt.Printf("db_r:%v\n", db_record)
+
+			// output log
+			fmt.Printf("ip=%s&accname=%s&pukkey=%s&deviceid=%s\n",
+				ip_str, accname, pubkey, deviceid)
+
+			// max create count
+			if db_record.Count > uint(cl) {
+				resform.Code = 404
+				resform.Msg = "exceed max count"
+				fmt.Println("exceed max count")
+				break
+			}
+
+			// fetch postion for sender
+			db_status := DbStatus{Pos: 1}
+			if db_value, err := db.Get(db_status_key, nil); err != nil {
+				if err != errors.ErrNotFound {
+					resform.Code = 500
+					resform.Msg = err.Error()
+					break
+				}
+			} else {
+				json.Unmarshal(db_value, &db_status)
+			}
+			// sync pos
+			send_pos := db_status.Pos
+			if 9 == db_status.Pos {
+				db_status.Pos = 1
+			} else {
+				db_status.Pos += 1
+			}
+			db_value, _ := json.Marshal(db_status)
+			db.Put(db_status_key, db_value[:], nil)
+
+			// rpc create account
+			tc.SetDefultURL(url)
+			sender_na := common.Name(na + strconv.Itoa(send_pos))
+			fmt.Println("sender_na:", sender_na)
+			cn, _ := tc.GetNonce(sender_na)
+
+			if err, hash := createAccount(common.Name(accname), sender_na, cn,
+				common.HexToPubKey(pubkey), prikey, chain_id); err != nil {
+				resform.Code = 500
+				resform.Msg = err.Error()
+				break
+			} else {
+				resform.Msg = hash.String()
+				// count ip limit
+				db_record.Count += 1
+				db_value, _ := json.Marshal(db_record)
+				// save back db record
+				db.Put(db_key[:], db_value[:], nil)
+
+				fmt.Printf("trans sent, count:%v\n", db_record.Count)
+				break
+			}
+
+			// break at the end
+			//break
+		}
+
+		//http.NotFound(w,r)
+		b, err := json.Marshal(resform)
+		if err != nil {
+			http.Error(w, err.Error(), 502)
+		} else {
+			// everything ok return json
+			w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			if resform.Code != 200 {
+				http.Error(w, string(b), resform.Code)
+			} else {
+				w.Write(b)
+			}
+		}
+	})
+
+	//fmt.Println("listen and serve")
+	http.ListenAndServe(":12345", nil)
+}
